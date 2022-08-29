1149f09117d5ae93f6832b1cbeb0f5b289ac0099
pavan0523 <82623666+pavan0523@users.noreply.github.com>
2022-08-29 12:02:13 -0400

    Added test case to validate different password scenarios

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.service.PasswordService;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private PasswordService passwordService;
+
 	@Test
-	void contextLoads() {
+	public void validPasswordsTest() {
+		String[] validPasswords = { "a1234", "abc123", "abcd1", "abc123", "restful0" };
+		List<Object> asList = Arrays.asList(validPasswords);
+		for (Object validPassword : asList) {
+			assertTrue(passwordService.validatePassword((String) validPassword));
+
+		}
+	}
+
+	@Test
+	public void invalidPasswordsTest() {
+		String[] validPasswords = { "1234", "Abc123", "12345", "clipper123", "vn123123", "abcde12345678",
+				"ABCDE12345678", "hel%@123" };
+		List<Object> asList = Arrays.asList(validPasswords);
+		for (Object invalidPassword : asList) {
+			assertFalse(passwordService.validatePassword((String) invalidPassword));
+
+		}
 	}
 
 }
